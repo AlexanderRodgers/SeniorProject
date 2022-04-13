d5eb4de726f922a173f57f4bbf7aa6735a6457d6
AlexanderRodgers <alexedrodgers@gmail.com>
2022-04-13 12:57:09 -0700

    added some code for the project sign up

--- client/src/routes/signup/index.svelte
+++ client/src/routes/signup/index.svelte
@@ -1,25 +1,29 @@
 <script lang="ts">
 	import TextInput from '$lib/components/TextInput.svelte';
+	import { supabase } from '../../supabase/supabaseClient';
 
 	let firstName: string;
 	let lastName: string;
 	let email: string;
 	let password: string;
 	let password2: string;
+	let invalidCredentials = false;
 
-  const handleSubmit = (e) => {
-    console.log(e)
-    const formData: FormData = new FormData(e.target);
-    const data = {};
-    for (let field of formData) {
-      const [key, value] = field;
-      data[key] = value;
-    }
-    console.log(data);
-  }
+	$: enableButton = firstName && lastName && email && password && password2;
 
-	const testSubmit = () => {
-		console.log('Hello!');
+	const handleSubmit = async (e) => {
+		if (password === password2) {
+			const { user, error } = await supabase.auth.signIn({
+				email: email,
+				password: password
+			});
+			if (user === null) {
+
+			}
+			if (error === null) {
+				invalidCredentials = true;
+			}
+		}
 	};
 </script>
 
@@ -27,21 +31,29 @@
 
 <form on:submit|preventDefault={handleSubmit}>
 	<div class="pt-4 px-2">
-		<TextInput placeholder="Joe" label="First Name" bind:value={firstName}/>
+		<TextInput placeholder="Joe" label="First Name" bind:value={firstName} />
 	</div>
 	<div class="pt-4 px-2">
-		<TextInput placeholder="Joe" label="Last Name" bind:value={lastName}/>
+		<TextInput placeholder="Joe" label="Last Name" bind:value={lastName} />
 	</div>
 	<div class="pt-4 px-2">
-		<TextInput placeholder="joesmith@gmail.com" label="Email" bind:value={email}/>
+		<TextInput placeholder="joesmith@gmail.com" label="Email" bind:value={email} />
 	</div>
 	<div class="pt-4 px-2">
-		<TextInput placeholder="Password" label="Password *" type="password" bind:value={password}/>
+		<TextInput placeholder="Password" label="Password *" type="password" bind:value={password} />
 	</div>
 	<div class="py-4 px-2">
-		<TextInput placeholder="Password" label="Re-enter Password *" type="password" bind:value={password2} />
+		<TextInput
+			placeholder="Password"
+			label="Re-enter Password *"
+			type="password"
+			bind:value={password2}
+		/>
 	</div>
 	<div class="px-2">
-		<button type="submit" class="btn btn-primary w-full">Submit</button>
+		<button disabled={!enableButton} type="submit" class="btn btn-primary w-full">Submit</button>
+	</div>
+	<div>
+		<p hidden={invalidCredentials}>Invalid Credentials</p>
 	</div>
 </form>
---
+++ client/src/types/signup.ts
@@ -0,0 +1,9 @@
+export type SignUp = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  password2: string;
+};
+
+export default SignUp;
